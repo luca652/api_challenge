3d26c6de7bcc4c59c27bc9838474c277a0ee8a55
luca gastaldi <lucagastaldi82@gmail.com>
2023-11-09 14:19:03 +0000

    added API call

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -11,14 +11,4 @@ class MuseumsController < ApplicationController
       redirect_to root_path, status: :unprocessable_entity
     end
   end
-
-  def mapbox_api_call
-    require "json"
-    require "open-uri"
-
-    url = "https://api.mapbox.com/museums?lat=52.494857&lng=13.437641"
-    museums_json = URI.open(url).read
-    museums = JSON.parse(museums_json)
-    p museums
-  end
 end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,6 +6,7 @@ class RequestsController < ApplicationController
   def create
     @request = Request.new(request_params)
     if @request.save
+      mapbox_api_call(@request.latitude, @request.longitude)
     else
       render root_path, status: :unprocessable_entity
     end
@@ -16,5 +17,15 @@ class RequestsController < ApplicationController
 
   def request_params
     params.require(:request).permit(:address)
+  end
+
+  def mapbox_api_call(lat, long)
+    require "json"
+    require "open-uri"
+
+    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=-#{lat},#{long}&access_token=pk.eyJ1IjoibHVjYS1nYXN0YWxkaSIsImEiOiJjbGZsaHN5d3QwMWcxNDNtbWRqencxMmNsIn0.XlVywjCjDzociHEu2-N48Q"
+    museums_json = URI.open(url).read
+    museums = JSON.parse(museums_json)
+    p museums
   end
 end
