5aabc3d2a7717dbde980a47df798fb1146dae86b
luca gastaldi <lucagastaldi82@gmail.com>
2023-11-09 12:14:58 +0000

    added geocoding lines to museum model

---
+++ app/controllers/museums_controller.rb
@@ -0,0 +1,23 @@
+class MuseumsController < ApplicationController
+  def index
+    @museums = Museum.all
+  end
+
+  def create
+    @museum = Museum.new(museum_params)
+    if @museum.save
+    else
+      redirect_to root_path, status: :unprocessable_entity
+    end
+  end
+
+  def mapbox_api_call
+    require "json"
+    require "open-uri"
+
+    url = "https://api.mapbox.com/museums?lat=52.494857&lng=13.437641"
+    museums_json = URI.open(url).read
+    museums = JSON.parse(museums_json)
+    p museums
+  end
+end
